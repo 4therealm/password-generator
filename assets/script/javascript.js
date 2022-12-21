cf23234fb91304517c4ee0634368fbe70dd3e1b7
maxwell walters <118125584+4therealm@users.noreply.github.com>
2022-12-21 16:41:57 -0600

    started concat for strings

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -3,21 +3,31 @@ var generateBtn = document.querySelector("#generate");
 var numberOfChars;
 var okToUseSpecialChars;
 var finalPassword = "";
+var specialC = ['!, @, #, $, %, ^, &'];//are the special characters a string?
+var lowerCase = ['a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'];
+var upperCase = ['A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z'];
+var numbers = ['0, 1, 2, 3, 4, 5, 6, 7, 8, 9'];
+var passwordBase = "";
 
-// use global variables to list out all possible:
-   // special characters
-   // lower case letters 
-   // upper case letters 
-   // numbers 
 
 function askQuestions(){
   var response = prompt("How many characters?");
   numberOfChars = parseInt(response);
   okToUseSpecialChars = confirm("Do you want to use special characters?");
 }
+if (okToUseSpecialChars == true){
+  passwordBase = (specialC + lowerCase + upperCase + numbers);
+}
+else {
+  passwordBase = (lowerCase + upperCase + numbers)
+}
 
 
+
+if 
 // use a loop to grab a random characters x number of times
+//for loop 
+//for(var i = 0; i <var.length; i++)
 
 //   finalPassword += 
 
@@ -26,8 +36,26 @@ function askQuestions(){
 // Your work starts here
 function generatePassword(){
   askQuestions();
-
+  
+  
+  return
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Write password to the #password input
