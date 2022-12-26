80680d07221a94dd615f3c839adcea08939e489f
maxwell walters <118125584+4therealm@users.noreply.github.com>
2022-12-26 14:11:32 -0600

    My commits were not consistent in this because i was unsure what i was doing was right. coded the arrays for what to include in password based on prompts. added passwordCriteria function to clean up the generatePassword function. coded if statements to determine which arrays to concatenate. coded the random number generator to loop through the password array length. coded alert message if numberOfChars is not within specified range

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,62 +1,56 @@
-// Assignment Code
+//global variables
 var generateBtn = document.querySelector("#generate");
+
 var numberOfChars;
 var okToUseSpecialChars;
-var finalPassword = "";
-var specialC = ['!, @, #, $, %, ^, &'];//are the special characters a string?
-var lowerCase = ['a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'];
-var upperCase = ['A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z'];
-var numbers = ['0, 1, 2, 3, 4, 5, 6, 7, 8, 9'];
-var passwordBase = "";
+var okToUseLowercase;
+var okToUseUppercase;
+var okToUseNumbers;
+//global arrays
+var specialChars = ["!","@","#","$","%","^","&","*","(","(","_","-","+","=","{","}","[","]","|",";",":","<",",",">",".","?","/",];
+var numbers = ["1","2","3","4","5","6","7","8","9","0",];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 
+var passwordArray = []; //this is the var that will be changed based on user input.
+
+//look at w3 diffferent functions for String, arrays, booleans
 
 function askQuestions(){
   var response = prompt("How many characters?");
   numberOfChars = parseInt(response);
+if(numberOfChars < 8 || numberOfChars > 126 || isNaN(numberOfChars))
+  { 
+  alert("password must be between 8 and 26 characters, please try again")
+  generatePassword() 
+  return
+  }
   okToUseSpecialChars = confirm("Do you want to use special characters?");
-}
-if (okToUseSpecialChars == true){
-  passwordBase = (specialC + lowerCase + upperCase + numbers);
-}
-else {
-  passwordBase = (lowerCase + upperCase + numbers)
+  okToUseLowercase = confirm("Do you want to use lowercase letters?");
+  okToUseUppercase = confirm("Do you want to use uppercase letters?");
+  okToUseNumbers = confirm("Do you want to use numbers?");
 }
 
-
-
-if 
-// use a loop to grab a random characters x number of times
-//for loop 
-//for(var i = 0; i <var.length; i++)
-
-//   finalPassword += 
-
+function passwordCriteria(){
+  if(okToUseSpecialChars){passwordArray = passwordArray.concat(specialChars);}
+  if (okToUseLowercase){passwordArray = passwordArray.concat(lowerCase);}
+  if (okToUseUppercase){passwordArray = passwordArray.concat(upperCase);}
+  if (okToUseNumbers){passwordArray = passwordArray.concat(numbers);}
+  else {passwordArray = null}
+}
 
 
 // Your work starts here
 function generatePassword(){
-  askQuestions();
-  
-  
-  return
-}
+  askQuestions(); 
+  passwordCriteria()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var password = "";
+  for (i=0; i < numberOfChars; i++)
+   {password = password+passwordArray[Math.floor(Math.random()*passwordArray.length)]}
+ 
+  return password
+  }
 
 // Write password to the #password input
 function writePassword() {
